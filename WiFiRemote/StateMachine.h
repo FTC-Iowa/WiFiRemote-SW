c87c3f11711631d1a33759d6c022a76bb9adba16
EricMiddleton <ericmiddleton621@gmail.com>
2017-09-03 01:21:34 -0500

    Fixes to make it compile properly with Arduino

--- WiFiRemote/StateMachine.h
+++ WiFiRemote/StateMachine.h
@@ -1,183 +1,179 @@
 #ifndef STATE_MACHINE_H
 #define STATE_MACHINE_H
 
-template< class T > struct remove_reference      {typedef T type;};
-template< class T > struct remove_reference<T&>  {typedef T type;};
-template< class T > struct remove_reference<T&&> {typedef T type;};  
+#include <utility>
 
-template <typename T>
-typename remove_reference<T>::type&& move(T&& arg)
-{
-  return static_cast<typename remove_reference<T>::type&&>(arg);
-}
+//Placement new
+void * operator new (size_t size, void * ptr) { return ptr; }
 
-template<int MaxEdges> class StateBuilder;
-
-using InitFunc = void(*)();
-using UpdateFunc = void(*)();
-using DeinitFunc = void(*)();
-using TransitionFunc = bool(*)();
-
-using StateID = int;
-
-template<int MaxEdges>
-class State {
-public:
+namespace StateMachine {
+  template<int MaxEdges> class StateBuilder;
   
-  StateID getStateID() const {
-    return id;
-  }
-
-  void init() {
-    initFunc();
-  }
+  using InitFunc = void(*)();
+  using UpdateFunc = void(*)();
+  using DeinitFunc = void(*)();
+  using TransitionFunc = bool(*)();
   
-  int update() {
-    updateFunc();
+  using StateID = int;
+  
+  template<int MaxEdges>
+  class State {
+  public:
     
-    for(unsigned int i = 0; i < edgeCount; ++i) {
-      Edge *ePtr = &(edges[i]);
+    StateID getStateID() const {
+      return id;
+    }
+  
+    void init() {
+      initFunc();
+    }
+    
+    int update() {
+      updateFunc();
       
-      if(ePtr->tf()) {
-        deinitFunc();
+      for(unsigned int i = 0; i < edgeCount; ++i) {
+        Edge *ePtr = &(edges[i]);
         
-        return ePtr->toState;
+        if(ePtr->tf()) {
+          deinitFunc();
+          
+          return ePtr->toState;
+        }
+      }
+      
+      return -1;
+    }
+        
+  private:
+    friend class StateBuilder<MaxEdges>;
+    struct Edge {
+      StateID toState;
+      TransitionFunc tf;
+    };
+    
+    StateID id;
+    
+    InitFunc initFunc;
+    UpdateFunc updateFunc;
+    DeinitFunc deinitFunc;
+      
+    Edge edges[MaxEdges];
+    int edgeCount;
+    
+  };
+  
+  template<int MaxEdges>
+  class StateBuilder {
+  public:
+    
+    StateBuilder(StateID _id, InitFunc _initFunc, UpdateFunc _updateFunc, DeinitFunc _deinitFunc)
+    :  id{_id}
+    ,  initFunc{_initFunc}
+    ,  updateFunc{_updateFunc}
+    ,  deinitFunc{_deinitFunc}
+    ,  edgeCount{0} {
+    }
+    
+    StateBuilder& addTransition(StateID _toState, TransitionFunc _tf) {
+      edges[edgeCount].toState = _toState;  
+      edges[edgeCount].tf = _tf;
+      edgeCount++;
+      
+      return *this;
+    }
+    
+    State<MaxEdges> build() {
+      State<MaxEdges> s;
+      
+      s.id = id;
+      s.initFunc = initFunc;
+      s.updateFunc = updateFunc;
+      s.deinitFunc = deinitFunc;
+      s.edgeCount = edgeCount;
+      
+      for(unsigned int i = 0; i < edgeCount; ++i) {
+        s.edges[i].toState = edges[i].toState;
+  			s.edges[i].tf = edges[i].tf;
+      }
+  
+  		return s;
+    }
+    
+  private:
+  	StateID id;
+    
+    InitFunc initFunc;
+    UpdateFunc updateFunc;
+    DeinitFunc deinitFunc;
+    
+    struct {
+  		StateID toState;
+  		TransitionFunc tf;
+  	} edges[MaxEdges];
+    int edgeCount;
+  };
+  
+  template<int StateCount, int StateMaxEdges>
+  class FSM {
+  public:
+    FSM(State<StateMaxEdges>&& startState)
+      :  curState{startState.getStateID()}
+      ,  newState{true} {
+      
+      initState(std::move(startState));
+    }
+        
+    bool addState(State<StateMaxEdges>&& state) {
+      if(state.getStateID() >= StateCount) {
+        return false;
+      }
+      else {
+        initState(std::move(state));
+        
+        return true;
       }
     }
     
-    return -1;
-  }
+    void run() {
+      auto statePtr = getState(curState);
       
-private:
-  friend class StateBuilder<MaxEdges>;
-  struct Edge {
-    StateID toState;
-    TransitionFunc tf;
+      if(newState) {
+        statePtr->init();
+  			newState = false;
+      }
+      
+      auto nextState = statePtr->update();
+      if(nextState != -1) {
+        curState = nextState;
+        newState = true;
+      }
+    }
+      
+  private:
+    unsigned char states[StateCount * sizeof(State<StateMaxEdges>)];
+    
+    StateID curState;
+    bool newState;
+    
+    State<StateMaxEdges>* getState(StateID id) {
+      return &reinterpret_cast<State<StateMaxEdges>*>(states)[id];
+    }
+    
+    bool initState(State<StateMaxEdges>&& state) {
+      auto id = state.getStateID();
+      
+      if(id < 0 || id >= StateCount) {
+        return false;
+      }
+      else {
+        new (getState(id)) State<StateMaxEdges>{state};
+        
+        return true;
+      }
+    }
+    
   };
-  
-  StateID id;
-  
-  InitFunc initFunc;
-  UpdateFunc updateFunc;
-  DeinitFunc deinitFunc;
-    
-  Edge edges[MaxEdges];
-  int edgeCount;
-  
-};
-
-template<int MaxEdges>
-class StateBuilder {
-public:
-  
-  StateBuilder(StateID _id, InitFunc _initFunc, UpdateFunc _updateFunc, DeinitFunc _deinitFunc)
-  :  id{_id}
-  ,  initFunc{_initFunc}
-  ,  updateFunc{_updateFunc}
-  ,  deinitFunc{_deinitFunc}
-  ,  edgeCount{0} {
-  }
-  
-  StateBuilder& addTransition(StateID _toState, TransitionFunc _tf) {
-    edges[edgeCount].toState = _toState;  
-    edges[edgeCount].tf = _tf;
-    edgeCount++;
-    
-    return *this;
-  }
-  
-  State<MaxEdges> build() {
-    State<MaxEdges> s;
-    
-    s.id = id;
-    s.initFunc = initFunc;
-    s.updateFunc = updateFunc;
-    s.deinitFunc = deinitFunc;
-    s.edgeCount = edgeCount;
-    
-    for(unsigned int i = 0; i < edgeCount; ++i) {
-      s.edges[i].toState = edges[i].toState;
-			s.edges[i].tf = edges[i].tf;
-    }
-
-		return s;
-  }
-  
-private:
-	StateID id;
-  
-  InitFunc initFunc;
-  UpdateFunc updateFunc;
-  DeinitFunc deinitFunc;
-  
-  struct {
-		StateID toState;
-		TransitionFunc tf;
-	} edges[MaxEdges];
-  int edgeCount;
-};
-
-template<int StateCount, int StateMaxEdges>
-class StateMachine {
-public:
-  StateMachine(State<StateMaxEdges>&& startState)
-    :  curState{startState.getStateID()}
-    ,  newState{true} {
-    
-    initState(move(startState));
-  }
-      
-  bool addState(State<StateMaxEdges>&& state) {
-    if(state.getStateID() >= StateCount) {
-      return false;
-    }
-    else {
-      initState(move(state));
-      
-      return true;
-    }
-  }
-  
-  void run() {
-    auto statePtr = getState(curState);
-    
-    if(newState) {
-      statePtr->init();
-			newState = false;
-    }
-    
-    auto nextState = statePtr->update();
-    if(nextState != -1) {
-      curState = nextState;
-      newState = true;
-    }
-  }
-    
-private:
-  unsigned char states[StateCount * sizeof(State<StateMaxEdges>)];
-  
-  StateID curState;
-  bool newState;
-  
-  State<StateMaxEdges>* getState(StateID id) {
-    return &reinterpret_cast<State<StateMaxEdges>*>(states)[id];
-  }
-  
-  bool initState(State<StateMaxEdges>&& state) {
-    auto id = state.getStateID();
-    
-    if(id < 0 || id >= StateCount) {
-      return false;
-    }
-    else {
-      new (getState(id)) State<StateMaxEdges>{state};
-      
-      return true;
-    }
-  }
-  
-};
-
+}
 
 #endif
 
--- WiFiRemote/WiFiRemote.ino
+++ WiFiRemote/WiFiRemote.ino
@@ -1,9 +1,30 @@
 #include "StateMachine.h"
 
+using namespace StateMachine;
+
+const int StateCount = 5;
+const int EdgeCount = 2;
+
+const int StateStart = 0,
+  StateConfigPage = 1,
+  StateConnectToAP = 2,
+  StateWaitForButton = 3,
+  StateSendButtonEvent = 4;
+
+FSM<StateCount, EdgeCount> fsm( StateBuilder<EdgeCount>{StateStart,
+  []() { /*Read buttons*/ },
+  []() { /*Do Nothing*/ },
+  []() { /*Do Nothing*/ } }
+  .addTransition(StateConfigPage,
+    []() { /*If buttons held*/ return false; } )
+  .addTransition(StateConnectToAP,
+    []() { /*If buttons not held*/ return false; } )
+  .build() );
+
 void setup() {
   
 }
 
 void loop() {
-  
+  fsm.run();
 }
